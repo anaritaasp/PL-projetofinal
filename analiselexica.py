8d94f5001cb21867953649a96b847342d38bad70
anaritaasp <ritaa3008@gmail.com>
2023-04-11 12:36:10 +0100

    capture for almost every word in grammar completed - only for portuguese words left to do

--- analiselexica.py
+++ analiselexica.py
@@ -4,11 +4,32 @@ import ply.lex as lex
 # List of token names. 
 
 tokens = (
-    'word:', #is the +base word, always appears in the beggining of a new line followed by the character ':'
-    '-word', #is the type of the base word, always appears in a new line if the line contains the character '-'
-    'PT_SENTENCE', #the portuguese sentence appears after more than \t before the end of the current line  
+    'baseword', #is the +base word, always appears in the beggining of a new line followed by the character ':'
+    'prefix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' before the word
+    'suffix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' after the word
+    'normalword', # an english word to be translated (doesn't have a type)
+    'portugueseTranslation', #the portuguese sentence appears after more than \t before the end of the current line  
 )
 
+def t_baseword(t): #example: dividends:
+    r'\w+:'
+    return t
+
+def t_prefix_word(t): #example:   - policy 
+    r'^(\s)+-+(\s)+(\w)+'
+    return t
+
+def t_suffix_word(t): #example:  operating -
+    r'^[\t ]+(\w)+[\t ]+-'
+    return t
+
+def t_normalword(t):#example: dole 
+    r'^(\w)+[\t ]'
+    return t
+
+#def t_portugueseTranslation(t):
+#    r'\w+ +-'
+#    return t
 
 def t_error(t):
     print(f"Carácter ilegal {t.value[0]}")
@@ -21,7 +42,6 @@ with open('dic-finance-en.pt.txt', 'r') as file:
 
 lexer.input(data)
 
-print (data)
 
-#while tok := lexer.token():
-#    print(tok)
+while tok := lexer.token():
+   print(tok)
