fe12270e6d4d9e46a782c218a77eb4b7dc22c10c
GuiSSMartins <guissmartins2013@gmail.com>
2023-04-16 06:16:40 +0100

    Novas regras (completar outras regras com pormenores que encontrar)

--- analiselexica.py
+++ analiselexica.py
@@ -1,40 +1,71 @@
 
 import ply.lex as lex
 
+states = (
+    ('ptsearch','exclusive'),
+)
+
 # List of token names. 
 
 tokens = (
+    'initial_letter', #is when we have a letter wihch finishes with '\n', example: A
     'baseword', #is the +base word, always appears in the beggining of a new line followed by the character ':'
     'prefix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' before the word
+    'middle_word', #is the type of the base word, always appears in a new line if the line contains the character '-' in the middle the word
     'suffix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' after the word
+    'final_word', # example: shift-
     'normalword', # an english word to be translated (doesn't have a type)
     'portugueseTranslation', #the portuguese sentence appears after more than \t before the end of the current line  
 )
 
-def t_baseword(t): #example: dividends:
-    r'\w+:'
+def t_initial_letter(t): #example: A\n
+    r'\w\s*\n'
     return t
 
-def t_prefix_word(t): #example:   - policy 
-    r'^(\s)+-+(\s)+(\w)+'
+def t_normalword(t):#example: dole (do ot lex example)     OU    yearly report
+    r'\w[\w-]*(\s\w[\w-]*)*(\s\([^\)]*\))?\s{2}\s*'
+    t.lexer.push_state('ptsearch')
     return t
 
-def t_suffix_word(t): #example:  operating -
-    r'^[\t ]+(\w)+[\t ]+-'
+def t_baseword(t): #example: automatic data: \n  OU  administration:         administração (f)
+    r'\w[\s\w-]*:'
+    t.lexer.push_state('ptsearch')
     return t
 
-def t_normalword(t):#example: dole 
-    r'^(\w)+[\t ]'
+# The third word is facultative!!!!!
+# Pode incluir outros casos como:    to-rule   (to -)
+
+def t_prefix_word(t): #example:   - of responsibilities (ROF)
+    r'(\s)+-\s\w[\w-]+(\s\w[\w-]+)?(\s\([^\)]*\))?\s{2}\s*'
+    t.lexer.push_state('ptsearch')
     return t
 
-#def t_portugueseTranslation(t):
-#    r'\w+ +-'
-#    return t
+def t_middle_word(t): #example:  value - tax (VAT)         OR        value - (VA)   
+    r'(\s)+\w[\w-]+\s-(\s\w[\w-]+)?(\s\([^\)]*\))?\s{2}\s*'
+    t.lexer.push_state('ptsearch')
+    return t
 
-def t_error(t):
-    print(f"Carácter ilegal {t.value[0]}")
-    t.lexer.skip(1)
+def t_suffix_word(t): #example:  value tax - (VTA)
+    r'(\s)+\w[\w-]+\s\w[\w-]+\s-(\s\([^\)]*\))?\s{2}\s*'
+    t.lexer.push_state('ptsearch')
+    return t
+
+def t_final_word(t): #example:  shift-
+    r'(\s)+\w[\w-]+\s{2}\s*'
+    t.lexer.push_state('ptsearch')
+    return t
+
+def t_ptsearch_portugueseTranslation(t): # includes () í ,
+    r'[^\n]*\n(\s{10}[^\n]*\n)*'
+    t.lexer.pop_state()
+    return t
     
+t_ANY_ignore = ""
+
+def t_ANY_error(t):
+    print(f"Caracter ilegal {t.value[0]}")
+    t.lexer.skip(1)
+
 lexer = lex.lex()
 
 with open('dic-finance-en.pt.txt', 'r') as file:
@@ -42,6 +73,9 @@ with open('dic-finance-en.pt.txt', 'r') as file:
 
 lexer.input(data)
 
+#print(lexer.token())
 
+#'''
 while tok := lexer.token():
-   print(tok)
+    print(tok)
+#'''
