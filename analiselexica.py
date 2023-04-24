c24bbd8d523ff184c59e191f7059dc19f1451e2b
GuiSSMartins <guissmartins2013@gmail.com>
2023-04-24 18:40:28 +0100

    Novos tokens
    Co-authored-by: Ana Rita Poças <anaritaasp@users.noreply.github.com>
    Co-authored-by: João Braga <LykiFyar@users.noreply.github.com>

--- analiselexica.py
+++ analiselexica.py
@@ -11,11 +11,16 @@ tokens = (
     'initial_letter', #is when we have a letter wihch finishes with '\n', example: A
     'baseword', #is the +base word, always appears in the beggining of a new line followed by the character ':'
     'prefix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' before the word
-    'middle_word', #is the type of the base word, always appears in a new line if the line contains the character '-' in the middle the word
+    'middle1_word', #is the type of the base word, always appears in a new line if the line contains the character '-' in the middle the word
+    'middle2_word', #is the type of the base word, always appears in a new line if the line contains the character '-' in the middle the word
     'suffix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' after the word
-    'final_word', # example: shift-
+    'double_word', #example: - base -
+    'prefix_error_word', # example: -structuring
+    'middle_error_word', #exameple: semi-costs
+    'suffix_error_word', # example: shift-
     'normalword', # an english word to be translated (doesn't have a type)
     'portugueseTranslation', #the portuguese sentence appears after more than \t before the end of the current line  
+    'portugueseTranslationError', # example: treinamento (mj dentro da indústria
     'paragraph',
 )
 
@@ -36,28 +41,69 @@ def t_baseword(t): #example: automatic data: \n  OU  administration:         adm
 # The third word is facultative!!!!!
 # Pode incluir outros casos como:    to-rule   (to -)
 
-def t_prefix_word(t): #example:   - of responsibilities (ROF)
-    r'(\s)+-\s\w[\w-]+(\s\w[\w-]+)?(\s\([^\)]*\))?\s{2}\s*'
+def t_prefix_word(t): #example:   - of responsibilities (ROF)   OR   - of responsibilities rof (ROFR)
+    r'\s+-\s\w[\w-]*(\s\w[\w-]*)?(\s\w[\w-]*)?(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
     t.lexer.push_state('ptsearch')
     return t
 
-def t_middle_word(t): #example:  value - tax (VAT)         OR        value - (VA)   
-    r'(\s)+\w[\w-]+\s-(\s\w[\w-]+)?(\s\([^\)]*\))?\s{2}\s*'
+def t_middle1_word(t): #example:  value - tax (VAT)         OR        value - (VA)   OR    value - Tax tax (VATT)
+    r'\s\w[\w-]*\s-(\s\w[\w-]*)?(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
     t.lexer.push_state('ptsearch')
     return t
 
-def t_suffix_word(t): #example:  value tax - (VTA)
-    r'(\s)+\w[\w-]+\s\w[\w-]+\s-(\s\([^\)]*\))?\s{2}\s*'
+def t_middle2_word(t): #example:  value tax - (VAT)         OR    value Tax - tax (VTAT)
+    r'\s\w[\w-]*\s\w[\w-]*\s-(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
     t.lexer.push_state('ptsearch')
     return t
 
-def t_final_word(t): #example:  shift-
-    r'(\s)+\w[\w-]+\s{2}\s*'
+def t_suffix_word(t): #example:  value tax final - (VTFA)
+    r'\s\w[\w-]*\s\w[\w-]*\s\w[\w-]*\s-(\s\([^\)]*\))?\s{2}\s*'
+    t.lexer.push_state('ptsearch')
+    return t
+
+def t_double_word(t): #example: - base -
+    r'\s\-\s\w[\w-]*\s\-'
+    t.lexer.push_state('ptsearch')
+    return t
+
+'''
+ - measurement                medição (f) de trabalho
+ clerical-measurement (CWM)   medição (f) de trabalho administrativo
+'''
+'''
+def t_continue_word(t):
+    r''
+    t.lexer.push_state('ptsearch')
+    return t
+'''
+
+# ERRO DE FORMATO
+def t_prefix_error_word(t) : # example: -structuring
+    # r'\s+-\w[\w-]*(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
+    r'\s+-\w+\s{2}\s*'
+    t.lexer.push_state('ptsearch')
+    return t
+
+# ERRO DE FORMATO
+def middle_error_word(t): #exameple: semi-costs
+    r'\s+\w+-\w+\s{2}\s*'
+    t.lexer.push_state('ptsearch')
+    return t
+
+# ERRO DE FORMATO
+def t_suffix_error_word(t): #example:  shift-
+    r'\s+\w+-\s{2}\s*'
     t.lexer.push_state('ptsearch')
     return t
 
 def t_ptsearch_portugueseTranslation(t): # includes () í ,
     r'[^\n]*\n(\s{10}[^\n]*\n)*'
+    t.lexer.pop_state()
+    return t
+
+# ERRO DE FORMATO
+def t_ptsearch_portugueseTranslationError(t): # example: treinamento (mj dentro da indústria
+    r'[^\n]*\n(\s{10}[^\)\n]*\n)*'
     t.lexer.pop_state()
     return t
 
