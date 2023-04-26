ecf6116a8cd6b252a412fe2b83ba7cd8cf3785bd
GuiSSMartins <guissmartins2013@gmail.com>
2023-04-26 16:05:43 +0100

    Avanços no léxico e sintático
    Co-authored-by: Ana Rita Poças <anaritaasp@users.noreply.github.com>
    Co-authored-by: João Braga <LykiFyar@users.noreply.github.com>

--- analiselexica.py
+++ analiselexica.py
@@ -10,11 +10,14 @@ states = (
 tokens = (
     'initial_letter', #is when we have a letter wihch finishes with '\n', example: A
     'baseword', #is the +base word, always appears in the beggining of a new line followed by the character ':'
+    'baseword_error', #example: worth
     'prefix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' before the word
     'middle1_word', #is the type of the base word, always appears in a new line if the line contains the character '-' in the middle the word
+    'middle1_word_error', #example:  sales -\n
     'middle2_word', #is the type of the base word, always appears in a new line if the line contains the character '-' in the middle the word
     'suffix_word', #is the type of the base word, always appears in a new line if the line contains the character '-' after the word
     'double_word', #example: - base -
+    'no_word', #example: engineering
     'prefix_error_word', # example: -structuring
     'middle_error_word', #exameple: semi-costs
     'suffix_error_word', # example: shift-
@@ -24,17 +27,27 @@ tokens = (
     'paragraph',
 )
 
+def t_paragraph(t):
+    r'\n'
+    pass
+
 def t_initial_letter(t): #example: A\n
-    r'\w\s*\n'
+    r'\w[ \r\t\f]*\n'
     return t
 
 def t_normalword(t):#example: dole (do ot lex example)     OU    yearly report
-    r'\w[\w-]*(\s\w[\w-]*)*(\s\([^\)]*\))?\s{2}\s*'
+    r'\w[\w\'-]*([ \r\t\f]\w[\w\-]*)*([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
 def t_baseword(t): #example: automatic data: \n  OU  administration:         administração (f)
-    r'\w[\s\w-]*:'
+    r'\w[ \r\t\f\w-]*:'
+    t.lexer.push_state('ptsearch')
+    return t
+
+# Falta os : no final
+def t_baseword_error(t): #example: worth\n
+    r'\w+\n'
     t.lexer.push_state('ptsearch')
     return t
 
@@ -42,27 +55,38 @@ def t_baseword(t): #example: automatic data: \n  OU  administration:         adm
 # Pode incluir outros casos como:    to-rule   (to -)
 
 def t_prefix_word(t): #example:   - of responsibilities (ROF)   OR   - of responsibilities rof (ROFR)
-    r'\s+-\s\w[\w-]*(\s\w[\w-]*)?(\s\w[\w-]*)?(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
+    r'[ \r\t\f]+-[ \r\t\f]\w[\w-]*([ \r\t\f]\w[\w-]*)?([ \r\t\f]\w[\w-]*)?([ \r\t\f]\w[\w-]*)?([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
 def t_middle1_word(t): #example:  value - tax (VAT)         OR        value - (VA)   OR    value - Tax tax (VATT)
-    r'\s\w[\w-]*\s-(\s\w[\w-]*)?(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
+    r'[ \r\t\f]+\w[\w-]*[ \r\t\f]-([ \r\t\f]\w[\w-]*)?([ \r\t\f]\w[\w-]*)?([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
+    t.lexer.push_state('ptsearch')
+    return t
+
+def t_middle1_word_error(t): #example:  sales -\n
+    r'[ \r\t\f]+\w[\w-]*[ \r\t\f]-([ \r\t\f]\w[\w-]*)?([ \r\t\f]\w[\w-]*)?([ \r\t\f]\([^\)]*\))?\n'
     t.lexer.push_state('ptsearch')
     return t
 
 def t_middle2_word(t): #example:  value tax - (VAT)         OR    value Tax - tax (VTAT)
-    r'\s\w[\w-]*\s\w[\w-]*\s-(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
+    r'[ \r\t\f]+\w[\w-]*[ \r\t\f]\w[\w-]*[ \r\t\f]-([ \r\t\f]\w[\w-]*)?([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
 def t_suffix_word(t): #example:  value tax final - (VTFA)
-    r'\s\w[\w-]*\s\w[\w-]*\s\w[\w-]*\s-(\s\([^\)]*\))?\s{2}\s*'
+    r'[ \r\t\f]+\w[\w-]*[ \r\t\f]\w[\w-]*[ \r\t\f]\w[\w-]*[ \r\t\f]-([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
 def t_double_word(t): #example: - base -
-    r'\s\-\s\w[\w-]*\s\-'
+    r'[ \r\t\f]+\-[ \r\t\f]\w[\w-]*[ \r\t\f]\-'
+    t.lexer.push_state('ptsearch')
+    return t
+
+# às vezes pode n
+def t_no_word(t): # diferença em relação ao normalword: contém espaço no início
+    r'[ \r\t\f]+\w+([ \r\t\f]\w[\w-]*)*([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
@@ -79,37 +103,33 @@ def t_continue_word(t):
 
 # ERRO DE FORMATO
 def t_prefix_error_word(t) : # example: -structuring
-    # r'\s+-\w[\w-]*(\s\w[\w-]*)?(\s\([^\)]*\))?\s{2}\s*'
-    r'\s+-\w+\s{2}\s*'
+    # r'[ \r\t\f]+-\w[\w-]*([ \r\t\f]\w[\w-]*)?([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
+    r'[ \r\t\f]+\-\w+([ \r\t\f]\w[\w-]*)*([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
 # ERRO DE FORMATO
-def middle_error_word(t): #exameple: semi-costs
-    r'\s+\w+-\w+\s{2}\s*'
+def t_middle_error_word(t): #exameple: semi-costs
+    r'[ \r\t\f]+\w+\-\w+([ \r\t\f]\w[\w-]*)*([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
 # ERRO DE FORMATO
 def t_suffix_error_word(t): #example:  shift-
-    r'\s+\w+-\s{2}\s*'
+    r'[ \r\t\f]+\w+-([ \r\t\f]\w[\w-]*)*([ \r\t\f]\([^\)]*\))?[ \r\t\f]{2}[ \r\t\f]*'
     t.lexer.push_state('ptsearch')
     return t
 
 def t_ptsearch_portugueseTranslation(t): # includes () í ,
-    r'[^\n]*\n(\s{10}[^\n]*\n)*'
+    r'[^\n]*\n([ \r\t\f]{10}[^\n]*\n)*'
     t.lexer.pop_state()
     return t
 
 # ERRO DE FORMATO
 def t_ptsearch_portugueseTranslationError(t): # example: treinamento (mj dentro da indústria
-    r'[^\n]*\n(\s{10}[^\)\n]*\n)*'
+    r'[^\n]*\n([ \r\t\f]{10}[^\)\n]*\n)*'
     t.lexer.pop_state()
     return t
-
-def t_paragraph(t):
-    r'\n'
-    pass
     
 t_ANY_ignore = ""
 
