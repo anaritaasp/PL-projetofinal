2fea5556615ab0e21d81fa797daeb29713d475ee
GuiSSMartins <guissmartins2013@gmail.com>
2023-05-19 09:55:11 +0100

    Tentativa

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from collections import OrderedDict
-import sys
-import os
 
 from analisesintatica import dicionario
 
 
 def buildFile():
+    file = open("final.txt", "w")
     for letter in dicionario:
         bases = dicionario[letter]
         for base in bases:
             (base_word, base_translation, dict_base) = bases[base]
-            string_base = "+base " + base_word
+            string_base = "+base " + base_word + "\n"
             if base_translation != "":
-                string_base = string_base + " " + base_translation
+                string_long_base = "EN " + base_word + "\nPT " + base_translation + "\n\n"
+                file.write(string_long_base)
             dict_base = OrderedDict(reversed(list(dict_base.items())))
             for english in dict_base:
+                file.write("EN " + english + "\n" + string_base)
                 portuguese = dict_base[english]
+                file.write("PT " + portuguese + "\n\n")
 def main():
     print()
     print("########################################################")
